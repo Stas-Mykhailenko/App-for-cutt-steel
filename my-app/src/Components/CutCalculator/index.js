5fcf301598f2fb318a9f0941003260c145692fff
Stas-Mykhailenko <stas1110900011>
2023-08-28 21:32:13 +0200

    add componts

---
+++ my-app/src/Components/CutCalculator/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import  '../CutCalculator/style.css'
+function CutCalculator() {
+  return(
+	<div className="body">Hellow</div>
+  )
+	
+  
+ 
+}
+export default CutCalculator;

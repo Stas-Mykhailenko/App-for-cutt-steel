e1d59dc9fc2a4293df5ac2a30b2c12889cac1d50
Stas-Mykhailenko <stas1110900011>
2023-09-03 17:58:27 +0200

    add dispatch

--- my-app/src/Components/NameInput/index.js
+++ my-app/src/Components/NameInput/index.js
@@ -1,13 +1,14 @@
 import {useState} from 'react';
-
+import {increment} from "../../features/counter/counterSlice"
+import { useSelector, useDispatch } from 'react-redux'
 
 function NameInput() {
   const [message, setMessage] = useState('');
+  const dispatch = useDispatch() 
   const handleChange = event => {
     setMessage(event.target.value);
-    console.log('value is:', event.target.value);
   };
-    
+  console.log('value is:', message);
 	return (
 <div className='input-container'>
       <h1 className='input-label'>Наименование профиля:</h1>
@@ -17,6 +18,7 @@ function NameInput() {
         step='any'
         placeholder="Введите название профиля..."
         required
+        onBlur={() => dispatch(increment(message))}
         onChange={handleChange}
         value={message}
       />

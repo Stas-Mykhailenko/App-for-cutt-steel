fcad7066a936f424b768c7d1bd0eb2082f0ff21c
Stas-Mykhailenko <stas1110900011>
2023-09-08 18:05:01 +0200

    add library export

--- my-app/src/Components/UploadExelButton/index.js
+++ my-app/src/Components/UploadExelButton/index.js
@@ -1,11 +1,59 @@
+import React from "react";
+import ReactExport from "react-data-export";
+
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
+
 function UploadExelButton() {
+  const dataSet1 = [
+    {
+      name: "Johson",
+      amount: 30000,
+      sex: "M",
+      is_married: true,
+    },
+    {
+      name: "Monika",
+      amount: 355000,
+      sex: "F",
+      is_married: false,
+    },
+    {
+      name: "John",
+      amount: 250000,
+      sex: "M",
+      is_married: false,
+    },
+    {
+      name: "Josef",
+      amount: 450500,
+      sex: "M",
+      is_married: true,
+    },
+  ];
+
   return (
     <div>
-      <input
-        className="submit-button btn-film"
-        type="button"
-        value="Скачать раскрой порезки металла Exel"
-      />
+      <ExcelFile
+        element={
+          <input
+            className="submit-button btn-film"
+            type="button"
+            value="Скачать раскрой порезки металла Exel"
+          />
+        }
+      >
+        <ExcelSheet data={dataSet1} name="Employees">
+          <ExcelColumn label="Name" value="name" />
+          <ExcelColumn label="Wallet Money" value="amount" />
+          <ExcelColumn label="Gender" value="sex" />
+          <ExcelColumn
+            label="Marital Status"
+            value={(col) => (col.is_married ? "Married" : "Single")}
+          />
+        </ExcelSheet>
+      </ExcelFile>
     </div>
   );
 }

5cebcd4ddb4bc7ebfda30717fdf254c7afbf6fe6
Stas-Mykhailenko <stas1110900011>
2023-09-17 21:04:05 +0200

    add time and export

--- my-app/src/Components/UploadExelButton/index.js
+++ my-app/src/Components/UploadExelButton/index.js
@@ -1,52 +1,58 @@
-// import React from "react";
-// import ReactExport from "react-data-export";
-
+import React from "react";
+import * as Filesaver from "file-saver";
+import XLSX from "sheetjs-style";
+import moment from "moment";
 // const ExcelFile = ReactExport.ExcelFile;
 // const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 // const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
 function UploadExelButton() {
-  // const dataSet1 = [
-  //   {
-  //     nameFirstColum: "6 m",
-  //     nameSecondColum: "64x40",
-  //     nameThirdColum: "2 шт",
-  //     nameFouthColum: "300 mm",
-  //   },
-  //   {
-  //     nameFirstColum: "6 m",
-  //     nameSecondColum: "64x40",
-  //     nameThirdColum: "2 шт",
-  //     nameFouthColum: "300 mm",
-  //   },
-  //   {
-  //     nameFirstColum: "6 m",
-  //     nameSecondColum: "64x40",
-  //     nameThirdColum: "2 шт",
-  //     nameFouthColum: "300 mm",
-  //   },
-  //   {
-  //     nameFirstColum: "6 m",
-  //     nameSecondColum: "64x40",
-  //     nameThirdColum: "2 шт",
-  //     nameFouthColum: "300 mm",
-  //   },
-  // ];
+  const dataSet1 = [
+    {
+      Заготовка: "6 m",
+      nameSecondColum: "64x40",
+      nameThirdColum: "2 шт",
+      nameFouthColum: "300 mm",
+    },
+    {
+      nameFirstColum: "6 m",
+      nameSecondColum: "64x40",
+      nameThirdColum: "2 шт",
+      nameFouthColum: "300 mm",
+    },
+    {
+      nameFirstColum: "6 m",
+      nameSecondColum: "64x40",
+      nameThirdColum: "2 шт",
+      nameFouthColum: "300 mm",
+    },
+    {
+      nameFirstColum: "6 m",
+      nameSecondColum: "64x40",
+      nameThirdColum: "2 шт",
+      nameFouthColum: "300 mm",
+    },
+  ];
 
+  const fileType =
+    "applicaction/vnd.openxlmformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+  const fileExtension = ".xlsx";
+  const fileName = moment().format("ll LT");
+  const exportToExcel = async () => {
+    const ws = XLSX.utils.json_to_sheet(dataSet1);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const exelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([exelBuffer], { type: fileType });
+    Filesaver.saveAs(data, fileName + fileExtension);
+  };
   return (
     <div>
-      {/* <ExcelFile element={<button>Download Data</button>}>
-        <ExcelSheet data={dataSet1} name="Employees">
-          <ExcelColumn nameFirstColum="Name" value="name" />
-          <ExcelColumn label="Wallet Money" value="amount" />
-          <ExcelColumn label="Gender" value="sex" />
-          <ExcelColumn label="Gender" value="sex" />
-          <ExcelColumn
-            label="Marital Status"
-            value={(col) => (col.is_married ? "Married" : "Single")}
-          />
-        </ExcelSheet>
-      </ExcelFile> */}
+      <input
+        className="submit-button btn-film"
+        type="button"
+        value="Скачать список остатков Exel"
+        onClick={(e) => exportToExcel(fileName)}
+      />
     </div>
   );
 }
